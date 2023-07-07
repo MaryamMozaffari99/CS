036f84ddfedb4eefc9b292117e679e50f2a6c804
MaryamMozaffari99@gmail.com <108430847+MaryamMozaffari99@users.noreply.github.com>
2023-07-07 19:07:52 +0330

    GetInput

--- GetInput/GetInput/Program.cs
+++ GetInput/GetInput/Program.cs
@@ -12,7 +12,19 @@ namespace GetInput
             string nameToLookFor = Console.ReadLine();
             Console.WriteLine($"Looking for {nameToLookFor} ... ");
 
+            for (int i = 0; i < names.Length; i++)
+            {
+                if(names[i] == nameToLookFor)
+                {
+                    Console.WriteLine($"Found {nameToLookFor} at the index of {i}. ");
+                    break;
+                }
 
+                if (i == names.Length - 1)
+                {
+                    Console.WriteLine($"could not find the {nameToLookFor} ...");
+                }
+            }
         }
     }
 }
