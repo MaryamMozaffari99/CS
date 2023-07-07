145a3929b29242c53df8cb1f8d2df906826544c7
MaryamMozaffari99@gmail.com <108430847+MaryamMozaffari99@users.noreply.github.com>
2023-07-07 13:20:32 +0330

    WhileLoopUpdate

--- WhileLoop/WhileLoop/Program.cs
+++ WhileLoop/WhileLoop/Program.cs
@@ -6,10 +6,16 @@ namespace WhileLoop
     {
         static void Main()
         {
-            while (true)
+            int age = 1;
+
+            while (age < 18)
             {
-                Console.WriteLine("We are looping ...");
+                Console.WriteLine("You are {age} years old, stay in school");
+                // this is the same as age = age + 1;
+                age += 1;
+               // age = age + 1;  
             }
+            Console.WriteLine("Now you are allowed to find a real job, or go to the college"); 
         }
     }
 }
