afeedce0e0a229f8fdcb02bac211cb7e5bd3f49a
MaryamMozaffari99@gmail.com <108430847+MaryamMozaffari99@users.noreply.github.com>
2023-07-07 12:42:38 +0330

    SwitchandEnum

---
+++ SwitchandEnums/SwitchandEnums/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SwitchandEnums
+{
+    internal class Program
+    {
+        enum groupSize
+        {
+            small, 
+            medium,
+            big
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            //int numberOfStudents = 10;
+
+            //string groupSize = "small";
+
+            //switch(numberOfStudents)
+
+            groupSize size = groupSize.small;   
+
+
+            switch (size)
+            {
+                //case 10:
+                case "small":
+                    Console.WriteLine("Hey!We are going to the small facility");
+                    break;
+                //case 30:
+                case "medium":
+                    Console.WriteLine("Hey!We are going to the medium facility");
+                    break;
+                //case 100:
+                case "big":
+                    Console.WriteLine("Hey!We are going to the big facility");
+                    break;
+                default:
+                    Console.WriteLine("Let's plan where to go");
+                    break;
+            }
+        }
+    }
+}
