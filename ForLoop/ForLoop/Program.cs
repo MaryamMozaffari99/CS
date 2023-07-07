36b1fa5be6749837539bd6d744c49df110977169
MaryamMozaffari99@gmail.com <108430847+MaryamMozaffari99@users.noreply.github.com>
2023-07-07 13:30:10 +0330

    ForLoop

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ForLoop
+{
+     static class Program
+    {
+        static void Main()
+        {
+            int age = 1;
+
+            while (age < 18)
+            {
+                Console.WriteLine($"You are {age} years old, stay in school");
+                age += 1;
+            }
+            Console.WriteLine("========================================================");
+            //age ++ is the same as age += 1 ;
+            for (int Age = 0; Age < 18; Age ++)
+            {
+                Console.WriteLine($"You are {Age} years old, stay in school");
+            }
+
+            Console.WriteLine("Now you are allowed to find a real job, or go to the " +
+                "college");
+        }
+    }
+}
