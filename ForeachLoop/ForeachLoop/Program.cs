28af884831e9ca6698711b8698bad7b4f0d12ca4
MaryamMozaffari99@gmail.com <108430847+MaryamMozaffari99@users.noreply.github.com>
2023-07-07 16:56:11 +0330

    ForEach

---
+++ ForeachLoop/ForeachLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForeachLoop
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] names = {"Bob", "Alex", "Adam", "Kate", "Steve"};
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine("================================================================");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
