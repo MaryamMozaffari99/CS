773bae8ea48f897d497e3e0d2fd423039f2f17fc
MaryamMozaffari99@gmail.com <108430847+MaryamMozaffari99@users.noreply.github.com>
2023-07-08 11:28:49 +0330

    RockPapperScissors.

---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] moves = { "Rock", "Paper", "Scissors" };
+            for (int i = 0; i < 10; i++)
+            {
+                Random rd = new Random();
+                int computerChoice = rd.Next(0, moves.Length);
+                Console.WriteLine($"the compuer picked: {moves[computerChoice]}");
+
+               // string playerMove = Console.ReadLine();
+
+
+
+
+            }
+        }
+    }
+}
