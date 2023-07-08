5e0c4c3383790e395d0680e5ee5e3604089fbb24
MaryamMozaffari99@gmail.com <108430847+MaryamMozaffari99@users.noreply.github.com>
2023-07-08 12:10:28 +0330

    RockPapperScissors

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -7,18 +7,43 @@ namespace RockPaperScissors
         static void Main()
         {
             string[] moves = { "Rock", "Paper", "Scissors" };
-            for (int i = 0; i < 10; i++)
+             
+            Random rd = new Random();
+            string computerMove = moves[rd.Next(0, moves.Length)];
+            
+            //Console.WriteLine($"the compuer picked: {moves[computerMove]}");
+
+            Console.WriteLine($"Please pick your move : {moves[0]}, {moves[1]}, {moves[2]}");
+            string playerMove = Console.ReadLine();
+
+            while ( playerMove != moves[0] && playerMove != moves[1] && playerMove != moves[2] ) 
             {
-                Random rd = new Random();
-                int computerChoice = rd.Next(0, moves.Length);
-                Console.WriteLine($"the compuer picked: {moves[computerChoice]}");
+                Console.WriteLine($"{playerMove} is not a valid move, Please pick: {moves[0]}, {moves[1]}, {moves[2]}");
+                playerMove = Console.ReadLine(); 
+            }
+            Console.WriteLine($"Your pick ; {playerMove} , computer pick:{computerMove}  ");
 
-               // string playerMove = Console.ReadLine();
+            if (computerMove == playerMove)
+            {
+                Console.WriteLine($"This is the tie!HAHAHA");
+            }
+            else
+            {
+                bool playerWinConditionOne = playerMove == moves[0] && computerMove == moves[2];
+                bool playerWinConditionTwo = playerMove == moves[1] && computerMove == moves[0];
+                bool playerWinConditionThree = playerMove == moves[2] && computerMove == moves[1];
 
-
-
+                if (playerWinConditionOne || playerWinConditionTwo || playerWinConditionThree)
+                {
+                    Console.WriteLine("You Win!");
+                }
+                else
+                {
+                    Console.WriteLine("You Lose.");
+                }
 
             }
+           
         }
     }
 }
